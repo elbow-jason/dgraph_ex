b94f4b95ae3f6aa75da4c16e030b8b06c4ac535f
Jason Goldberger <jlgoldb2@asu.edu>
2017-08-08 12:32:11 -0700

    refactored block test with clean_format

--- test/dgraph_ex_block_test.exs
+++ test/dgraph_ex_block_test.exs
@@ -2,43 +2,64 @@ defmodule DgraphEx.BlockTest do
   use ExUnit.Case
   doctest DgraphEx.Query.Block
   import DgraphEx
-  alias DgraphEx.Query.Block
+  import TestHelpers
 
   test "block function renders simple example" do
-    b = {
+    assert render({
       block(:jason, func: eq(:name, "Jason")), {
         :name
       }
-    }
-    assert b |> render == "{ jason(func: eq(name, \"Jason\")) { name } }"
+    }) == clean_format("""
+      {
+        jason(func: eq(name, \"Jason\")) {
+          name
+        }
+      }
+    """)
   end
 
   test "block function renders a complex example" do
-    b = {
+    assert render({
       block(:Taraji_films_by_genre_count, func: uid(:G), orderdesc: val(:G)), {
         aliased(:genres, block(:genre, orderdesc: val(:C))), {
            aliased(:genre_name, :name@en)
         },
         film_name: :name@en,
       }
-    }
-    assert b |> render == "{ Taraji_films_by_genre_count(func: uid(G), orderdesc: val(G)) { genres: genre(orderdesc: val(C)) { genre_name: name@en } film_name: name@en } }"
+    }) == clean_format("""
+      {
+        Taraji_films_by_genre_count(func: uid(G), orderdesc: val(G)) {
+          genres: genre(orderdesc: val(C)) {
+            genre_name: name@en
+          } 
+          film_name: name@en
+        }
+      }
+    """)
   end
 
   test "block literal requires `aliased/2` when first member of a tuple a keyword pair" do
-    b = {
+    assert render({
       block(:Taraji_films_by_genre_count, func: uid(:G), orderdesc: val(:G)), {
         aliased(:genres, block(:genre, orderdesc: val(:C))), {
            aliased(:genre_name, :name@en)
         },
         film_name: :name@en,
       }
-    }
-    assert render(b) == "{ Taraji_films_by_genre_count(func: uid(G), orderdesc: val(G)) { genres: genre(orderdesc: val(C)) { genre_name: name@en } film_name: name@en } }"
+    }) == clean_format("""
+      {
+        Taraji_films_by_genre_count(func: uid(G), orderdesc: val(G)) {
+          genres: genre(orderdesc: val(C)) {
+            genre_name: name@en
+          }
+          film_name: name@en
+        }
+      }
+    """)
   end
 
   test "block literal does not require `aliased/2` when first member of a tuple is not a keyword pair" do
-    b = {
+    assert render({
       block(:Taraji_films_by_genre_count, func: uid(:G), orderdesc: val(:G)), {
         :film_budget,
         block(:genres, orderdesc: val(:C)), {
@@ -47,46 +68,81 @@ defmodule DgraphEx.BlockTest do
         },
         film_name: :name@en,
       }
-    }
-    assert render(b) ==  "{ Taraji_films_by_genre_count(func: uid(G), orderdesc: val(G)) { film_budget genres(orderdesc: val(C)) { other_field genre_name: name@en } film_name: name@en } }"
+    }) == clean_format("""
+      {
+        Taraji_films_by_genre_count(func: uid(G), orderdesc: val(G)) {
+          film_budget
+          genres(orderdesc: val(C)) {
+            other_field
+            genre_name: name@en
+          }
+          film_name: name@en
+        }
+      }
+    """)
+    
   end
 
   test "empty block" do
-    assert Block.render({}) == "{ }"
+    assert render({}) == "{ }"
   end
 
   test "block with simple terms" do
-    assert Block.render({
+    assert render({
       :name,
       :address,
-    }) == "{ name address }"
+    }) == clean_format("""
+      {
+        name
+        address
+      }
+    """)
   end
 
   test "block with keyword term alias (at the end only)" do
-    assert Block.render({
+    assert render({
       :name,
       :address,
       age: :years,
-    }) == "{ name address age: years }"
+    }) == clean_format("""
+      {
+        name
+        address
+        age: years
+      }
+    """)
   end
-  
 
   test "block with keyword multiple term alias (at the end only)" do
-    assert Block.render({
+    assert render({
       :name,
       :address,
       age: :years,
       favorite_color: :fav_color,
-    }) == "{ name address age: years favorite_color: fav_color }"
+    }) == clean_format("""
+      {
+        name
+        address
+        age: years
+        favorite_color: fav_color
+      }
+    """) 
   end
 
   test "block with expressions as keywords works" do
-    assert Block.render({
+    assert render({
       :name,
       :address,
       age: count(:years),
       friends: sum(val(:F)),
-    }) == "{ name address age: count(years) friends: sum(val(F)) }"
+    }) == clean_format("""
+      {
+        name
+        address
+        age: count(years)
+        friends: sum(val(F))
+      }
+    """)
   end
 
 
